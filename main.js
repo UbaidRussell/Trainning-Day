accfda013ae07244c742126d81427862ee8d9485
Ubaid <urussell0802@gmail.com>
2022-08-02 05:30:08 -0400

    +COMPETITOR.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const name = 'Nala';
-
 // The scope of `random` is too loose 
-const random = Math.floor(Math.random() * 3);
+
 
 const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+
   if (random === 0) {
     return 'Marathon';
   } else if (random === 1) {
@@ -33,7 +34,6 @@ const logEvent = (name, event) => {
 };
 
 const logTime = (name, days) => {
-
   console.log(`${name}'s time to train is: ${days} days`);
 };
 
@@ -41,6 +41,15 @@ const event = getRandEvent();
 const days = getTrainingDays(event);
 // Define a `name` variable. Use it as an argument after updating logEvent and logTime 
 
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+ 
+
+
 
 logEvent(name, event);
 logTime(name, days);
+
+logEvent(name2, event2);
+logTime(name2, days2);
